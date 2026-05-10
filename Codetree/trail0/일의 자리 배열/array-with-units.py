d42ffe0c43808ac16f302a5d5f8b8c748ef6fca3
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-05-10 21:35:21 +0900

    [Passed] 일의 자리 배열 (47ms, 16MB)

---
+++ Codetree/trail0/일의 자리 배열/array-with-units.py
@@ -0,0 +1,10 @@
+arr = [0 for _ in range(10)]
+arr[0], arr[1] = map(int, input().split())
+
+for i in range(2, len(arr)):
+    num = arr[i-2] + arr[i-1]
+    if num >= 10:
+        num = num - 10
+    arr[i] = num
+    
+print(*arr)
