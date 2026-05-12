ceee1a967d3547ecdfb023599f737bbba7b417a6
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-05-12 11:52:49 +0900

    [Passed] 100점 도달하기 (47ms, 16MB)

---
+++ Codetree/trail0/100점 도달하기/reach-100-point.py
@@ -0,0 +1,16 @@
+score = int(input())
+
+while score<=100:
+    if score >= 90:
+        print('A', end=' ')
+    elif score >= 80:
+        print('B', end=' ')
+    elif score >= 70:
+        print('C', end=' ')
+    elif score >= 60:
+        print('D', end=' ')
+    else:
+        print('F', end=' ')
+    
+    score += 1
+
