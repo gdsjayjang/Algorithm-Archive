af5bbb0aa83f124b156427d92b6eadab12e4e3e1
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-05-20 22:47:48 +0900

    [Passed] 약수가 세 개인 수 (61ms, 17MB)

--- Codetree/trail0/약수가 세 개인 수/numbers-with-three-divisors.py
+++ Codetree/trail0/약수가 세 개인 수/numbers-with-three-divisors.py
@@ -5,7 +5,7 @@ res = 0
 for i in range(start, end+1):
     cnt = 0
     for j in range(1, i+1):
-        if ((i) % j) == 0:
+        if (i % j) == 0:
             cnt += 1
     if cnt == 3:
         res += 1
