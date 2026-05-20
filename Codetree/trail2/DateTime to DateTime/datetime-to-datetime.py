31ad162e1531796e06343490e227f46f729a34e3
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-05-20 22:32:03 +0900

    [Passed] DateTime to DateTime (47ms, 16MB)

--- Codetree/trail2/DateTime to DateTime/datetime-to-datetime.py
+++ Codetree/trail2/DateTime to DateTime/datetime-to-datetime.py
@@ -16,3 +16,11 @@ else:
     results = day_min + hour_min + minute
 
     print(results)
+
+# another
+# diff = (a * 24 * 60 + b * 60 + c) - (11 * 24 * 60 + 11 * 60 + 11)
+
+# if diff < 0:
+#     print(-1)
+# else:
+#     print(diff)
