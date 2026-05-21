3615477911db888913a86a22ff18508eadd4acef
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-05-22 00:25:51 +0900

    [Passed] 상하로 반복하며 출력 (48ms, 16MB)

---
+++ Codetree/trail0/상하로 반복하며 출력/print-in-up-down-reverse.py
@@ -0,0 +1,19 @@
+N = int(input())
+arr_2d = [[0 for _ in range(N)] for _ in range(N)]
+
+for j in range(N):
+    if j % 2 != 0:
+        num = 1
+        for i in range(N-1, -1, -1):
+            arr_2d[i][j] = num
+            num += 1
+    if j % 2 == 0:
+        num = 1
+        for i in range(N):
+            arr_2d[i][j] = num
+            num += 1
+
+for i in range(N):
+    for j in range(N):
+        print(arr_2d[i][j], end='')
+    print()
