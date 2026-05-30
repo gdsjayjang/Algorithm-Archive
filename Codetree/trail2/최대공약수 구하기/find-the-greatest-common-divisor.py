64cab2678cdff06a1d197e0122f2631864d438a4
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-05-30 15:26:10 +0900

    [Passed] 최대공약수 구하기 (48ms, 16MB)

---
+++ Codetree/trail2/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+
+# Please write your code here.
+if n < m:
+    n, m = m, n
+
+def gcd(n, m):
+    while m != 0:
+        temp = m
+        m = n % m   # remainder
+        n = temp
+
+    return n
+
+result = gcd(n, m)
+print(result)
