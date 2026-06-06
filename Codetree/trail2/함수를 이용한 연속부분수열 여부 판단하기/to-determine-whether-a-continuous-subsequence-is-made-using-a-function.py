14792872ecdb0f4ea8255f966bdc12aea4312a4e
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-06-06 18:39:28 +0900

    [Passed] 함수를 이용한 연속부분수열 여부 판단하기 (48ms, 16MB)

--- Codetree/trail2/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ Codetree/trail2/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -27,7 +27,8 @@ def func(n1, n2, a, b):
 result = func(n1, n2, a, b)
 print(result)
 
-# others
+
+# Sol.1
 # def func(n1, n2, a, b):
 #     for i in range(n1 - n2 + 1):
 #         if a[i:i+n2] == b:
@@ -37,3 +38,29 @@ print(result)
 
 # result = func(n1, n2, a, b)
 # print(result)
+
+
+
+# Sol.2
+# # n번째 인덱스부터 시작하는 n2길이의 a수열과
+# # b수열이 완전히 일치하는지 확인합니다..
+# def is_same(n):
+#     for i in range(n2):
+#         if a[i + n] != b[i]:
+#             return False
+
+#     return True
+
+# # b가 a의 연속부분수열인지 확인합니다.
+# def is_subsequence():
+#     for i in range(n1 - n2 + 1):
+#         if is_same(i):
+#             return True
+    
+#     return False
+
+
+# if is_subsequence():
+#     print("Yes")
+# else:
+#     print("No")
