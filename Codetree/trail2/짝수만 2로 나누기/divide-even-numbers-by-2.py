38f6bec05de4592477c97efa3dfa3f9afc959902
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-06-07 19:00:10 +0900

    [Passed] 짝수만 2로 나누기 (49ms, 16MB)

--- Codetree/trail2/짝수만 2로 나누기/divide-even-numbers-by-2.py
+++ Codetree/trail2/짝수만 2로 나누기/divide-even-numbers-by-2.py
@@ -2,8 +2,11 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 # Please write your code here.
-for i in range(n):
-    if arr[i] % 2 == 0:
-        arr[i] = int(arr[i] / 2)
+def func(arr):
+    for i in range(n):
+        if arr[i] % 2 == 0:
+            arr[i] = int(arr[i] / 2)
 
-print(*arr)
+    print(*arr)
+
+func(arr)
