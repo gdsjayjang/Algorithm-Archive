7104c9ae2ad936706866f914002d136bb954e068
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-06-08 14:48:08 +0900

    [Passed] 트로미노 (77ms, 18MB)

--- Codetree/trail4/트로미노/tromino.py
+++ Codetree/trail4/트로미노/tromino.py
@@ -2,54 +2,87 @@ n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
-# max_m 부터
-# ㅡ 모양
-max_m_m = 0
+ans = 0
+
+# 일자 막대 ㅡ
 for i in range(n):
-    for j in range(m-2):
-        temp_m_m = grid[i][j] + grid[i][j+1] + grid[i][j+2]
-        if max_m_m < temp_m_m:
-            max_m_m = temp_m_m
-# max_m 부터
-# ㅣ 모양
-max_m_l = 0
-for j in range(m):
-    for i in range(n-2):
-        temp_m_l = grid[i][j] + grid[i+1][j] + grid[i+2][j]
-        if max_m_l < temp_m_l:
-            max_m_l = temp_m_l
-max_r_s = 0
-# ㄴ 모양
-for i in range(n-1):
-    for j in range(m-1):
-        temp_r_s = grid[i][j] + grid[i+1][j] + grid[i+1][j+1]
-        if max_r_s < temp_r_s:
-            max_r_s = temp_r_s
-max_r_r = 0
-# ㄱ 모양
-for i in range(n-1):
-    for j in range(m-1):
-        temp_r_r = grid[i][j] + grid[i][j+1] + grid[i+1][j+1]
-        if max_r_r < temp_r_r:
-            max_r_r = temp_r_r
-max_r_rs = 0
-# ㄴ 뒤집은 모양
-for i in range(n-1):
-    for j in range(m-1):
-        temp_r_rs = grid[i][j+1] + grid[i+1][j+1] + grid[i+1][j]
-        if max_r_rs < temp_r_rs:
-            max_r_rs = temp_r_rs    
-max_r_rr = 0
-# ㄱ 뒤집은 모양
-for i in range(n-1):
-    for j in range(m-1):
-        temp_r_rr = grid[i][j] + grid[i][j+1] + grid[i+1][j]
-        if max_r_rr < temp_r_rr:
-            max_r_rr = temp_r_rr
+    for j in range(m - 2):
+        ans = max(ans, grid[i][j] + grid[i][j+1] + grid[i][j+2])
 
-print(max(max_m_m, max_m_l, max_r_r, max_r_s, max_r_rr, max_r_rs))
+# 일자 막대 ㅣ
+for i in range(n - 2):
+    for j in range(m):
+        ans = max(ans, grid[i][j] + grid[i+1][j] + grid[i+2][j])
 
-# sol1.
+# ㄴ모양 (2x2 영역 안에서 3칸 고르기)
+for i in range(n - 1):
+    for j in range(m - 1):
+        # 2x2 안의 4개 원소 정의
+        v1 = grid[i][j]      # 왼쪽 위
+        v2 = grid[i][j+1]    # 오른쪽 위
+        v3 = grid[i+1][j]    # 왼쪽 아래
+        v4 = grid[i+1][j+1]  # 오른쪽 아래
+        
+        # 4칸 중 3칸을 고르는 것은, 1칸을 제외하는 것과 같음
+        sum1 = v1 + v2 + v3  # v4 제외 (┌ 모양)
+        sum2 = v1 + v2 + v4  # v3 제외 (┐ 모양)
+        sum3 = v1 + v3 + v4  # v2 제외 (└ 모양)
+        sum4 = v2 + v3 + v4  # v1 제외 (┘ 모양)
+        
+        ans = max(ans, sum1, sum2, sum3, sum4)
+
+print(ans)
+
+# my old sol 1.
+# # max_m 부터
+# # ㅡ 모양
+# max_m_m = 0
+# for i in range(n):
+#     for j in range(m-2):
+#         temp_m_m = grid[i][j] + grid[i][j+1] + grid[i][j+2]
+#         if max_m_m < temp_m_m:
+#             max_m_m = temp_m_m
+# # max_m 부터
+# # ㅣ 모양
+# max_m_l = 0
+# for j in range(m):
+#     for i in range(n-2):
+#         temp_m_l = grid[i][j] + grid[i+1][j] + grid[i+2][j]
+#         if max_m_l < temp_m_l:
+#             max_m_l = temp_m_l
+# max_r_s = 0
+# # ㄴ 모양
+# for i in range(n-1):
+#     for j in range(m-1):
+#         temp_r_s = grid[i][j] + grid[i+1][j] + grid[i+1][j+1]
+#         if max_r_s < temp_r_s:
+#             max_r_s = temp_r_s
+# max_r_r = 0
+# # ㄱ 모양
+# for i in range(n-1):
+#     for j in range(m-1):
+#         temp_r_r = grid[i][j] + grid[i][j+1] + grid[i+1][j+1]
+#         if max_r_r < temp_r_r:
+#             max_r_r = temp_r_r
+# max_r_rs = 0
+# # ㄴ 뒤집은 모양
+# for i in range(n-1):
+#     for j in range(m-1):
+#         temp_r_rs = grid[i][j+1] + grid[i+1][j+1] + grid[i+1][j]
+#         if max_r_rs < temp_r_rs:
+#             max_r_rs = temp_r_rs    
+# max_r_rr = 0
+# # ㄱ 뒤집은 모양
+# for i in range(n-1):
+#     for j in range(m-1):
+#         temp_r_rr = grid[i][j] + grid[i][j+1] + grid[i+1][j]
+#         if max_r_rr < temp_r_rr:
+#             max_r_rr = temp_r_rr
+
+# print(max(max_m_m, max_m_l, max_r_r, max_r_s, max_r_rr, max_r_rs))
+
+
+# sol 2.
 # # 가능한 모든 모양을 전부 적어줍니다.
 # shapes = [
 #     [[1, 1, 0],
