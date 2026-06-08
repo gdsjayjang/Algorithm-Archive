470de1736c1f4334bf240fac8f5777c1b123d834
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-06-08 14:37:18 +0900

    [Passed] 트로미노 (96ms, 19MB)

--- Codetree/trail4/트로미노/tromino.py
+++ Codetree/trail4/트로미노/tromino.py
@@ -48,3 +48,61 @@ for i in range(n-1):
             max_r_rr = temp_r_rr
 
 print(max(max_m_m, max_m_l, max_r_r, max_r_s, max_r_rr, max_r_rs))
+
+# sol1.
+# # 가능한 모든 모양을 전부 적어줍니다.
+# shapes = [
+#     [[1, 1, 0],
+#     [1, 0, 0],
+#     [0, 0, 0]],
+
+#     [[1, 1, 0],
+#     [0, 1, 0],
+#     [0, 0, 0]],
+
+#     [[1, 0, 0],
+#     [1, 1, 0],
+#     [0, 0, 0]],
+
+#     [[0, 1, 0],
+#     [1, 1, 0],
+#     [0, 0, 0]],
+
+#     [[1, 1, 1],
+#     [0, 0, 0],
+#     [0, 0, 0]],
+
+#     [[1, 0, 0],
+#     [1, 0, 0],
+#     [1, 0, 0]],
+# ]
+
+# # 주어진 위치에 대하여 가능한 모든 모양을 탐색하며 최대 합을 반환합니다.
+# def get_max_sum(x, y):
+#     max_sum = 0
+#     for i in range(6):
+#         is_possible = True
+#         sum_of_nums = 0
+#         for dx in range(0, 3):
+#             for dy in range(0, 3):
+#                 if shapes[i][dx][dy] == 0:
+#                     continue
+#                 if x + dx >= n or y + dy >= m:
+#                     is_possible = False
+#                 else:
+#                     sum_of_nums += grid[x + dx][y + dy]
+        
+#         if is_possible:
+#             max_sum = max(max_sum, sum_of_nums)
+
+#     return max_sum
+
+
+# ans = 0
+
+# # 격자의 각 위치에 대하여 탐색하여줍니다.
+# for i in range(n):
+#     for j in range(m):
+#         ans = max(ans, get_max_sum(i, j))
+
+# print(ans)
