cb07c15bf644838a43d6d12db24c30ca8b8245ef
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-07-16 21:49:35 +0900

    [Wrong Answer] 별 그리기 (51ms, 16MB)

---
+++ Codetree/trail1/별 그리기/star-drawing.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+# Please write your code here.
+for i in range(n):
+    # 공백 2 1 0
+    print(' '*(n-i-1), end='')
+
+    # 별표 1 3 5 7
+    print('*' * (2*i+1), end='')
+
+    print()
+
+# j = 0, 1
+for j in range(n-1):
+    # 공백 1 2
+    print(' ' * (j+1), end='')
+
+    # 별표 3 1
+    print('*' * (n-2*j), end='')
+
+    print()
