17ad77cf2dcc649ea7a801e5735c2fc9f1a47d88
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-07-24 22:17:20 +0900

    [Passed] 별표 출력하기 13 (47ms, 16MB)

--- Codetree/trail1/별표 출력하기 13/print-start-13.py
+++ Codetree/trail1/별표 출력하기 13/print-start-13.py
@@ -2,13 +2,29 @@ n = int(input())
 
 for i in range(2*n):
     if i % 2 == 0:
-        idx = (2*n-i)// 2 #
         # idx should be 5, 4, 3, 2, 1
+        idx = (2*n-i)// 2 #
         for _ in range(idx):
             print('*', end=' ')
 
     else:
+        # idx should be 1, 2, 3, 4, 5
         idx = i // 2 + 1
         for _ in range(idx):
             print('*', end=' ')
     print()
+
+
+# # others
+# # 변수 선언 및 입력
+# n = int(input())
+
+# # i가 홀수인 경우 별을 1 + (i // 2)개, 짝수인 경우 n - (i // 2)개 출력합니다
+# for i in range(2 * n):
+#     if i % 2 == 1:
+#         for _ in range(1 + i // 2):
+#             print("* ", end="")
+#     else:
+#         for _ in range(n - i // 2):
+#             print("* ", end="")
+#     print()
