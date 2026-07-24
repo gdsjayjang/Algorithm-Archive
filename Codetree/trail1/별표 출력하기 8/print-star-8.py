3e5e2f2998fbbd67f6546d45d3b7262699dcf3ed
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-07-24 15:34:57 +0900

    [Passed] 별표 출력하기 8 (62ms, 16MB)

--- Codetree/trail1/별표 출력하기 8/print-star-8.py
+++ Codetree/trail1/별표 출력하기 8/print-star-8.py
@@ -6,3 +6,17 @@ for i in range(1, n+1):
     else:
         print('* ' * i, end=' ')
         print()
+
+
+# # others
+# # 변수 선언 및 입력
+# n = int(input())
+
+# #i가 짝수인 경우 별을 1개, 홀수인 경우 i+1개를 출력합니다
+# for i in range(n):
+#     if i % 2 == 0:
+#         print("*", end="")
+#     else:
+#         for _ in range(i + 1):
+#             print("*", end=" ")
+#     print()
