6ee1460a04698e23837adfb5b112b0b64a609b8d
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-08-03 17:46:59 +0900

    [Passed] 규칙에 맞는 출력 (46ms, 16MB)

--- Codetree/trail1/규칙에 맞는 출력/output-that-matches-the-rule.py
+++ Codetree/trail1/규칙에 맞는 출력/output-that-matches-the-rule.py
@@ -4,3 +4,11 @@ for i in range(1, n+1):
     for j in range(1, i+1):
         print(n-i+j, end=' ')
     print()
+
+
+# # others
+# # 숫자로 이루어진 삼각형을 출력합니다.
+# for i in range(n):
+#     for j in range(i + 1):
+#         print(n - i + j, end=" ")
+#     print()
