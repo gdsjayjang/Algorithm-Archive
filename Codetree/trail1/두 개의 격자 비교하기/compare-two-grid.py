46e4e1e5d3887e204ea3ce89fc525e891a07416c
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-08-05 16:00:15 +0900

    [Passed] 두 개의 격자 비교하기 (49ms, 16MB)

---
+++ Codetree/trail1/두 개의 격자 비교하기/compare-two-grid.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+
+arr1 = [list(map(int, input().split())) for _ in range(n)]
+arr2 = [list(map(int, input().split())) for _ in range(n)]
+res_arr = [[0 for _ in range(m)] for _ in range(n)]
+
+for i in range(n):
+    for j in range(m):
+        if arr1[i][j] != arr2[i][j]:
+            res_arr[i][j] = 1
+
+for row in res_arr:
+    print(*row)
