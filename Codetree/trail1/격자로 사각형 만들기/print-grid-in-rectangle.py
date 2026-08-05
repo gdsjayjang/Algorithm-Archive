5b6910fdd212937db62fc26f9b238e99dec38fcc
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-08-05 15:40:58 +0900

    [Passed] 격자로 사각형 만들기 (49ms, 16MB)

---
+++ Codetree/trail1/격자로 사각형 만들기/print-grid-in-rectangle.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+arr = [
+    [1 for _ in range(n)] for _ in range(n)
+]
+
+for i in range(1, n):
+    for j in range(1, n):
+        arr[i][j] = arr[i-1][j-1] + arr[i][j-1] + arr[i-1][j]
+
+for row in arr:
+    print(*row)
