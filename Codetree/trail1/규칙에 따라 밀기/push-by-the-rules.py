c6544087cd7d47a7c58ee659cf0bbb1c456ea6d4
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-08-06 10:16:51 +0900

    [Passed] 규칙에 따라 밀기 (54ms, 17MB)

--- Codetree/trail1/규칙에 따라 밀기/push-by-the-rules.py
+++ Codetree/trail1/규칙에 따라 밀기/push-by-the-rules.py
@@ -15,3 +15,24 @@ for i in range(len(com)):
         inp[0] = temp
 
 print(*inp, sep='')
+
+
+# # others
+# # 문자열을 입력받습니다.
+# string = input()
+# string2 = input()
+    
+# # 문자열의 길이를 구합니다.
+# leng = len(string)
+    
+# # 명령 문자열에서 제시하는 대로 행동합니다.
+# for elem in string2:
+#     if elem == 'L':
+#         # 명령 문자열이 L일 때에는 문자열을 왼쪽으로 한 칸 쉬프트합니다.
+#         string = string[1:] + string[0]
+#     else:
+#         # 명령 문자열이 R일 때에는 문자열을 오른쪽으로 한 칸 쉬프트합니다.
+#         string = string[leng - 1] + string[:leng - 1]
+
+# # 명령대로 쉬프트된 문자열을 출력합니다.
+# print(string)
