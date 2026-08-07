e906b8ec00c3a09e371c1b7d04c02f75f32c8e82
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-08-07 18:06:37 +0900

    [Passed] 행에 따라 다르게 출력 (50ms, 18MB)

--- Codetree/trail1/행에 따라 다르게 출력/output-defferently-each-row.py
+++ Codetree/trail1/행에 따라 다르게 출력/output-defferently-each-row.py
@@ -10,3 +10,19 @@ for i in range(n):
             print(cnt+1, end=' ')
             cnt += 2
     print()
+
+
+# # others
+# # 변수 선언 및 입력
+# n = int(input())
+# cnt = 0
+    
+# # 값을 조건대로 증가시켜 출력합니다.
+# for i in range(n):
+#     for j in range(n):
+#         if i % 2 == 0:
+#             cnt += 1
+#         else:
+#             cnt += 2
+#         print(cnt, end=" ")
+#     print()
