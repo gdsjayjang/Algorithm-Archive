26f07427adedd7e77027618c5098e80a0b4f178a
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-08-07 18:15:32 +0900

    [Passed] 좌우로 반복하며 출력 (50ms, 16MB)

--- Codetree/trail1/좌우로 반복하며 출력/print-in-left-right-reverse.py
+++ Codetree/trail1/좌우로 반복하며 출력/print-in-left-right-reverse.py
@@ -1,10 +1,9 @@
 n = int(input())
 
 for i in range(n):
-    if i % 2 == 0:
-        for j in range(1, n+1):
-            print(j, end='')
-    else:
-        for j in range(n):
-            print(n-j, end='')
+    for j in range(n):
+        if i % 2 == 0:
+            print(j + 1, end='')
+        else:
+            print(n - j, end='')
     print()
