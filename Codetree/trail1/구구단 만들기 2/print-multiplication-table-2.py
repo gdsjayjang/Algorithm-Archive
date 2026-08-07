a4069e6d6eea76597ef68601f4af8d9c0c3aba55
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-08-07 18:27:36 +0900

    [Passed] 구구단 만들기 2 (47ms, 16MB)

--- Codetree/trail1/구구단 만들기 2/print-multiplication-table-2.py
+++ Codetree/trail1/구구단 만들기 2/print-multiplication-table-2.py
@@ -7,3 +7,18 @@ for j in arr:
             print(f'{i} * {j} = {i*j} /', end=' ')
         else:
             print(f'{i} * {j} = {i*j}')
+
+
+# # others
+# # 변수 선언, 입력
+# inp = input()
+# arr = inp.split()
+# a, b = int(arr[0]), int(arr[1])
+
+# # 조건대로 구구단을 출력합니다.
+# for i in range (2, 9, 2):
+#     for j in range(b, a - 1, -1):
+#         print(f"{j} * {i} = {i * j}", end="")
+#         if j != a:
+#             print(" / ", end="")
+#     print()
