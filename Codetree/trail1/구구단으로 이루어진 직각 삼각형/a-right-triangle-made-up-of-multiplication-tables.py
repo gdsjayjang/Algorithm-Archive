0dc497c0661b2b29dd0a613b02e8443398faf7a9
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-08-08 16:21:10 +0900

    [Passed] 구구단으로 이루어진 직각 삼각형 (49ms, 16MB)

--- Codetree/trail1/구구단으로 이루어진 직각 삼각형/a-right-triangle-made-up-of-multiplication-tables.py
+++ Codetree/trail1/구구단으로 이루어진 직각 삼각형/a-right-triangle-made-up-of-multiplication-tables.py
@@ -7,3 +7,17 @@ for i in range(1, n+1):
         else:
             print(f'{i} * {j} = {i*j}', end='')
     print()
+
+
+# # others
+# # 변수 선언 및 입력
+# n = int(input())
+
+# # 구구단으로 이루어진 직각 삼각형을 출력합니다.
+# for i in range(1, n + 1):
+#     for j in range(1, n - i + 2):
+#         print(f"{i} * {j} = {i * j}", end="")
+        
+#         if j != (n - i + 1):
+#             print(" / ", end="")
+#     print()
