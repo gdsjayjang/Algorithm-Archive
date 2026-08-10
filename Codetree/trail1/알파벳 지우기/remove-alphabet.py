5f2d1e646cf434ff939c3f6c6cc1ef81a88c85b4
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-08-10 09:13:34 +0900

    [Runtime Error] 알파벳 지우기 (83ms, 19MB)

--- Codetree/trail1/알파벳 지우기/remove-alphabet.py
+++ Codetree/trail1/알파벳 지우기/remove-alphabet.py
@@ -6,10 +6,10 @@ arr_b = []
 
 for i in a:
     if i.isdigit():
-        arr_a.append(i)
+        arr_a.append(int(i))
 
 for j in b:
     if j.isdigit():
-        arr_b.append(j)
+        arr_b.append(int(j))
 
 print(''.join(arr_a) + ''.join(arr_b))
