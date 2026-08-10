3fd25c9234ae0a7904544e8ae1c2ae03c7c30cc5
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-08-10 09:14:54 +0900

    [Passed] 알파벳 지우기 (48ms, 16MB)

--- Codetree/trail1/알파벳 지우기/remove-alphabet.py
+++ Codetree/trail1/알파벳 지우기/remove-alphabet.py
@@ -6,10 +6,10 @@ arr_b = []
 
 for i in a:
     if i.isdigit():
-        arr_a.append(int(i))
+        arr_a.append(i)
 
 for j in b:
     if j.isdigit():
-        arr_b.append(int(j))
+        arr_b.append(j)
 
-print(''.join(arr_a) + ''.join(arr_b))
+print(int(''.join(arr_a)) + int(''.join(arr_b)))
