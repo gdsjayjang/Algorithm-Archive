b0234f8d16235f96330d82e59ab5892bfda9915e
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-08-11 09:38:55 +0900

    [Passed] 특정 규칙에 따른 숫자 출력 (50ms, 16MB)

--- Codetree/trail1/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.py
+++ Codetree/trail1/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.py
@@ -7,8 +7,11 @@ for i in range(n):
     
     for _ in range(n-i):
         print(num, end=' ')
-        num += 1
+        
+        if num == 9:
+            num = 1
+        else:
+            num += 1
     
-    if num > 9:
-        num = 1
+
     print()
