723aa83b589a6ef1a91b37c6e2a6ceaeadd78a8b
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-08-11 14:32:11 +0900

    [Passed] 나눗셈의 나머지 (47ms, 16MB)

--- Codetree/trail1/나눗셈의 나머지/remainder-of-division.py
+++ Codetree/trail1/나눗셈의 나머지/remainder-of-division.py
@@ -1,7 +1,7 @@
 a, b = map(int, input().split())
 
 arr = []
-while a >= 1:
+while a > 1:
     mod = a % b
     a = a // b # 몫
     arr.append(mod)
@@ -17,5 +17,5 @@ res = 0
 appear = list(dic.values())
 for i in appear:
     res += i**2
-    
+
 print(res)
