9d1fccfcc4535d1ab38bbde446123c754d74ed6d
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-08-11 09:36:33 +0900

    [Wrong Answer] 특정 규칙에 따른 숫자 출력 (48ms, 17MB)

--- Codetree/trail1/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.py
+++ Codetree/trail1/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.py
@@ -3,9 +3,12 @@ n = int(input())
 num = 1
 for i in range(n):
     for _ in range(i):
-        print('', end=' ')
+        print(' ', end=' ')
     
-    for _ in range(n-i)
+    for _ in range(n-i):
         print(num, end=' ')
+        num += 1
     
+    if num > 9:
+        num = 1
     print()
