11c667bdf6f3f01e9d6bb3dd5554f1d122935a87
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-08-12 10:05:27 +0900

    [Passed] 알파벳 출력하기 2 (47ms, 16MB)

--- Codetree/trail1/알파벳 출력하기 2/print-alphabet-2.py
+++ Codetree/trail1/알파벳 출력하기 2/print-alphabet-2.py
@@ -13,3 +13,20 @@ for i in range(n): # i = 0 1 2
         print(chr(init), end=' ')
         init += 1
     print()
+
+
+# # other
+# # 변수 선언 및 입력
+# n = int(input())
+# cnt = 'A'
+    
+# # 알파벳을 역삼각형 모양으로 출력합니다.
+# for i in range(n):
+#     for _ in range(i):
+#         print("  ", end="")
+#     for _ in range(n - i):
+#         print(cnt, end=" ")
+#         cnt = chr(ord(cnt) + 1)
+#         if ord(cnt) > ord('Z'):
+#             cnt = 'A'
+#     print()
