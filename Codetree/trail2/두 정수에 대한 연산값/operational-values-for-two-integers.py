c993224b3b6e978c839567d64a79a4330795beda
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-08-14 11:10:38 +0900

    [Passed] 두 정수에 대한 연산값 (48ms, 16MB)

--- Codetree/trail2/두 정수에 대한 연산값/operational-values-for-two-integers.py
+++ Codetree/trail2/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -4,10 +4,10 @@ def func(arr):
     if arr[0] > arr[1]:
         arr[0] += 25
         arr[1] *= 2
-
     else:
         arr[1] += 25
         arr[0] *= 2
+        
     return arr
 
 print(*func(arr))
