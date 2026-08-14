b0af8c1ebfbc981ef75580b00c0ec17cd0308739
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-08-14 11:30:43 +0900

    [Passed] 두 정수에 대한 연산값 2 (48ms, 16MB)

---
+++ Codetree/trail2/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
@@ -0,0 +1,15 @@
+a, b = map(int, input().split())
+
+# Please write your code here.
+def func(a, b):
+    if a < b:
+        a += 10
+        b *= 2
+    else:
+        a *= 2
+        b += 10
+    
+    return a, b
+
+a, b = func(a, b)
+print(a, b)
