f70011b2858b660e6b607ba946939abb58d4c4c6
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-08-14 11:04:20 +0900

    [Wrong Answer] 두 정수에 대한 연산값 (52ms, 16MB)

--- Codetree/trail2/두 정수에 대한 연산값/operational-values-for-two-integers.py
+++ Codetree/trail2/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -1,12 +1,13 @@
 a, b = map(int, input().split())
 
 def func(a, b):
-    big = max(a, b)
-    small = min(a, b)
+    big = max(a,b)
+    small = min(a,b)
+
     big += 25
     small *= 2
 
-    return big, small
+    return small, big
 
-big, small = func(a, b)
-print(big, small)
+small, big = func(a, b)
+print(small, big)
