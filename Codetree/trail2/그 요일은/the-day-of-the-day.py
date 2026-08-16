39b2bbeea4d48b32edfc024fb3047b9df36cb932
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-08-16 17:07:26 +0900

    [Passed] 그 요일은 (53ms, 17MB)

--- Codetree/trail2/그 요일은/the-day-of-the-day.py
+++ Codetree/trail2/그 요일은/the-day-of-the-day.py
@@ -8,7 +8,12 @@ days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 start_days = sum(month[:m1]) + d1
 end_days = sum(month[:m2]) + d2
 
-if A == 'Sun':
+diff = end_days - start_days
+div = diff  // 7
+mod = diff - div * 7
+
+
+if (days.index(A)) > mod:
     print((end_days - start_days) // 7)
 else:   
     print((end_days - start_days) // 7 + 1) 
