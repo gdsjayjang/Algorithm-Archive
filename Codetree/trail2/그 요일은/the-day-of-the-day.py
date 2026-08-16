12595d257ec10bf3d4c72a4c7d858d4c14128e25
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-08-16 17:10:43 +0900

    [Passed] 그 요일은 (46ms, 16MB)

--- Codetree/trail2/그 요일은/the-day-of-the-day.py
+++ Codetree/trail2/그 요일은/the-day-of-the-day.py
@@ -5,15 +5,70 @@ A = input()
 month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
+# 1월 1일부터 떨어진 날짜
 start_days = sum(month[:m1]) + d1
 end_days = sum(month[:m2]) + d2
 
+# 두 날짜 간 차이와 7로 나눴을 때의 몫과 나머지
 diff = end_days - start_days
 div = diff  // 7
 mod = diff - div * 7
 
-
+# days 인덱스를 통해 +1을 할지 말지
 if (days.index(A)) > mod:
     print((end_days - start_days) // 7)
 else:   
-    print((end_days - start_days) // 7 + 1) 
+    print((end_days - start_days) // 7 + 1)
+
+
+# # others
+# # 변수 선언 및 입력
+# m1, d1, m2, d2 = tuple(map(int, input().split()))
+# A = input()
+
+# def num_of_days(m, d):
+#     # 계산 편의를 위해 월마다 몇 일이 있는지를 적어줍니다. 
+#     days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+#     total_days = 0
+    
+#     # 1월부터 (m - 1)월 까지는 전부 꽉 채워져 있습니다.
+#     for i in range(1, m):
+#         total_days += days[i]
+    
+#     # m월의 경우에는 정확이 d일만 있습니다.
+#     total_days += d
+    
+#     return total_days
+
+
+# def num_of_day(s):
+#     # 간단한 비교를 위해 요일을 숫자로 나타내줍니다.
+#     if s == "Mon": 
+#         return 0
+#     elif s == "Tue": 
+#         return 1
+#     elif s == "Wed": 
+#         return 2
+#     elif s == "Thu": 
+#         return 3
+#     elif s == "Fri": 
+#         return 4
+#     elif s == "Sat": 
+#         return 5
+#     return 6
+
+
+# ans = 0   
+# start_date = num_of_days(m1, d1)
+# end_date = num_of_days(m2, d2)
+# cur_day = num_of_day("Mon")
+
+# for date in range(start_date, end_date + 1):
+#     # 오늘의 요일이 A요일과 같다면 정답에 추가합니다.
+#     if cur_day == num_of_day(A): 
+#         ans += 1
+
+#     cur_day = (cur_day + 1) % 7
+
+# # 출력
+# print(ans)
