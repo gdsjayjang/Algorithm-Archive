40fc79301f8c2109218b7b999202e44b8db256dd
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-08-19 21:28:18 +0900

    [Passed] 십진수와 이진수 2 (48ms, 16MB)

--- Codetree/trail2/십진수와 이진수 2/decimal-and-binary-number-2.py
+++ Codetree/trail2/십진수와 이진수 2/decimal-and-binary-number-2.py
@@ -15,9 +15,42 @@ for i in arr:
     k+= 1
 
 res = res * 17
+
+# 이진수 변환
 arr2 = []
 while res > 0:
     arr2.append(res % 2)
     res = res // 2
 
 print(*arr2[::-1], sep='')
+
+
+# # others
+# # 변수를 선언하고 이진수로 표현된 수를 입력받습니다.
+# binary = list(map(int, list(input())))
+# length = len(binary)
+
+# # 십진수로 변환합니다.
+# num = 0
+# for i in range(length):
+#     num = num * 2 + binary[i]
+
+# # 십진수에 17을 곱합니다.
+# num *= 17
+
+# digits = []
+
+# # 이진수로 변환합니다.
+# while True:
+#     if num < 2:
+#         digits.append(num)
+#         break
+
+#     digits.append(num % 2)
+#     num //= 2
+
+# # 이진수를 출력합니다.
+# # 뒤집은 다음에 출력해야 함에 유의합니다.
+# for digit in digits[::-1]:
+#     print(digit, end="")
+# print()
