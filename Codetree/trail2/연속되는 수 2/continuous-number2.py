5ffb37541c3ad8c60ef1c72ff3e4fd2869a17e24
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-08-20 10:17:18 +0900

    [Wrong Answer] 연속되는 수 2 (52ms, 16MB)

---
+++ Codetree/trail2/연속되는 수 2/continuous-number2.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+# Please write your code here.
+n_arr = len(arr)
+res = 0
+diff = 0
+for i in range(n_arr):
+    if i == 0 or arr[i] == arr[i-1]:
+        diff += 1
+    if res < diff:
+        res = diff+1
+        diff = 0
+
+print(res)
