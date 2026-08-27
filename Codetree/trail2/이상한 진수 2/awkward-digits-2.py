78f5c9c63d9007409663d5f20062f06c7691d70b
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-08-27 18:46:25 +0900

    [Passed] 이상한 진수 2 (49ms, 16MB)

--- Codetree/trail2/이상한 진수 2/awkward-digits-2.py
+++ Codetree/trail2/이상한 진수 2/awkward-digits-2.py
@@ -25,3 +25,32 @@ for i in range(n_arr):
     # arr 초기화
     arr_copy = arr.copy()
 print(ten_max)
+
+
+# # others
+# import sys
+
+# INT_MIN = -sys.maxsize
+
+# # 변수 선언 및 입력
+# binary = list(map(int, list(input())))
+# length = len(binary)
+
+# # 각 i번째 자릿수를 바꾸었을 때의 십진수 값을 구해줍니다.
+# ans = INT_MIN
+# for i in range(length):
+#     # i번째 자릿수를 바꿉니다.
+#     binary[i] = 1 - binary[i]
+    
+#     # 십진수로 변환합니다.
+#     num = 0
+#     for j in range(length):
+#         num = num * 2 + binary[j]
+    
+#     ans = max(ans, num)
+    
+#     # i번째 자릿수를 원래대로 돌려놓습니다.
+#     binary[i] = 1 - binary[i]
+
+# # 출력
+# print(ans)
