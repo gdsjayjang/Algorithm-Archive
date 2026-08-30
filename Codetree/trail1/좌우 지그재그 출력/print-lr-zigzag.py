9a8f6e6692e93a83ccd46c5c54e0baac17764baa
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-08-30 21:08:02 +0900

    [Passed] 좌우 지그재그 출력 (46ms, 16MB)

--- Codetree/trail1/좌우 지그재그 출력/print-lr-zigzag.py
+++ Codetree/trail1/좌우 지그재그 출력/print-lr-zigzag.py
@@ -9,3 +9,17 @@ for i in range(n):
         for j in range(n, 0, -1):
             print(i * n + j, end=' ')
     print()
+
+
+# # others
+# # 변수 선언 및 입력
+# n = int(input())
+    
+# # 좌우 지그재그로 출력합니다.
+# for i in range(n):
+#     for j in range(n):
+#         if i % 2 == 0:
+#             print((i * n) + j + 1, end=" ")
+#         else:
+#             print((i * n) + n - j, end=" ")
+#     print()
