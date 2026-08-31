dac36c40633f2b90799efc1f459c217376716c50
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-08-31 17:12:54 +0900

    [Passed] 모양대로 수 출력하기 (48ms, 16MB)

--- Codetree/trail1/모양대로 수 출력하기/print-out-numbers-in-specific-shape.py
+++ Codetree/trail1/모양대로 수 출력하기/print-out-numbers-in-specific-shape.py
@@ -6,3 +6,17 @@ for i in range(n):
     for k in range(n-i, 0, -1):
         print(k, end=' ')
     print()
+
+
+# # others
+# # 변수 선언 및 입력
+# n = int(input())
+    
+# # 숫자로 이루어진 삼각형을 출력합니다.
+# for i in range(n, 0, -1):
+#     for j in range(n, 0, -1):
+#         if j > i:
+#             print(" ", end=" ")
+#         else:
+#             print(j, end=" ")
+#     print()
