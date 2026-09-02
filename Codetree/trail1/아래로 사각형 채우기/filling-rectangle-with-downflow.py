a57fc0f9e6ca7a9a714425fa48260622c95f0030
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-09-02 20:15:53 +0900

    [Passed] 아래로 사각형 채우기 (55ms, 16MB)

--- Codetree/trail1/아래로 사각형 채우기/filling-rectangle-with-downflow.py
+++ Codetree/trail1/아래로 사각형 채우기/filling-rectangle-with-downflow.py
@@ -6,3 +6,27 @@ for i in range(1, n+1):
         print(res, end=' ')
         res += n
     print()
+
+
+# # others
+# # n을 입력받습니다.
+# n = int(input())
+# num = 1
+
+# # 2차원 배열을 구현합니다.
+# arr = [
+#     [0 for _ in range(n)]
+#     for _ in range(n)
+# ]
+    
+# # 배열의 숫자를 채웁니다.
+# for i in range(n):
+#     for j in range(n):
+#         arr[j][i] = num
+#         num += 1
+        
+# # 채워진 배열을 출력합니다.
+# for row in arr:
+#     for elem in row:
+#         print(elem, end=" ")
+#     print()
