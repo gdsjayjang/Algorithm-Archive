c1394b1005612ebde7227f2da68d2605c41535b6
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-09-07 16:02:18 +0900

    [Passed] 소문자와 숫자 (48ms, 16MB)

--- Codetree/trail1/소문자와 숫자/letter-and-number.py
+++ Codetree/trail1/소문자와 숫자/letter-and-number.py
@@ -1,19 +1,23 @@
 inp = input()
 n = len(inp)
-# print(ord('a')) # 97
-# print(ord('z')) # 122
-# print(ord('A')) # 65
-# print(ord('Z')) # 90
-# print(ord('0')) # 48
-# print(ord('1')) # 49
-# print(ord('9')) # 57
 
-# print(inp.lower())
 for i in range(n):
-    # 대문자
     if (ord(inp[i]) >= 65) and (ord(inp[i]) <= 90):
         print(inp[i].lower(), end='')
     elif (ord(inp[i]) >= 97) and (ord(inp[i]) <= 122):
         print(inp[i], end='')
     elif (ord(inp[i]) >= 48) and (ord(inp[i]) <= 57):
         print(inp[i], end='')
+
+
+# # others
+# # 문자열을 입력받습니다.
+# string = input()
+
+# # 문자를 하나하나 확인하여 알파벳일 경우 모두 소문자로, 숫자일 경우 그대로 출력합니다.
+# for elem in string:
+#     if(elem >= 'A' and elem <= 'Z') or (elem >= 'a' and elem <= 'z'):
+#         print(elem.lower(), end="")
+    
+#     if(elem >= '0' and elem <= '9'):
+#         print(elem, end="")
