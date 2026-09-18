a756e46271c18d4febf97793cf15585d4646dfc9
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-09-18 16:28:37 +0900

    [Passed] 재귀함수를 이용한 피보나치 수 (72ms, 19MB)

--- Codetree/trail2/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.py
+++ Codetree/trail2/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.py
@@ -2,9 +2,7 @@ N = int(input())
 
 # Please write your code here.
 def func(n):
-    if n == 1:
-        return 1
-    elif n == 2:
+    if n <= 2:
         return 1
     
     return func(n-2) + func(n-1)
