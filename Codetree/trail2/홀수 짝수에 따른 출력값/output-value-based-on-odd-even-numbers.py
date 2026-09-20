c40cc331d2653a23460989d5f1f002b8cab422a3
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-09-20 13:33:58 +0900

    [Passed] 홀수 짝수에 따른 출력값 (50ms, 16MB)

--- Codetree/trail2/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.py
+++ Codetree/trail2/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.py
@@ -2,13 +2,10 @@ N = int(input())
 
 # Please write your code here.
 def func(n):
-    sum = 0
-    if n % 2 == 1:
-        for i in range(1, n+1, 2):
-            sum += i
-    else:
-        for i in range(2, n+1, 2):
-            sum += i
-    return sum
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    return func(n-2) + n
 
 print(func(N))
