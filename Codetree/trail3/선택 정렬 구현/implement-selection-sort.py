bf02ea2128cf7989809507485de3eb9e8d826335
Yoonsuk Jang <gdsjayjang@gmail.com>
2026-09-22 22:10:26 +0900

    [Passed] 선택 정렬 구현 (53ms, 16MB)

---
+++ Codetree/trail3/선택 정렬 구현/implement-selection-sort.py
@@ -0,0 +1,14 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+for i in range(n-1):
+    min = i
+    for j in range(i+1, n):
+        if arr[j] < arr[min]:
+            min = j
+    temp = arr[i]
+    arr[i] = arr[min]
+    arr[min] = temp
+
+print(*arr, sep=' ')
